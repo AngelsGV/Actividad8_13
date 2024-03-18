c589dc87616ef56f55b0fc18d8cf418802081869
AngelsGV <angels.not99@gmail.com>
2024-03-18 13:07:41 +0100

    Initial commit

---
+++ src/Ejercicio8_12/Caja.java
@@ -0,0 +1,64 @@
+package Ejercicio8_12;
+
+public class Caja {
+    // Definición de parámetros
+    //Por el momento no no les pongo modificador de visibilidad,
+    // pero después debería poner protected para que las clases vecinas los puedan usar.
+    double ancho;
+    double alto;
+    double fondo;
+    String etiqueta;
+
+    //Uso double porque m y cm puede estar definido en decimal
+    Caja(double ancho, double alto, double fondo, String unidad ){//constructor caja
+//lo de la unidad lo he buscado. También lo podría hacer con unidad.toLowerCase().equals("cm"))
+        if (unidad.equalsIgnoreCase("cm")) {
+            this.ancho = ancho / 100;  // Convertir de cm a m
+            this.alto = alto / 100;
+            this.fondo = fondo / 100;
+        } else if (unidad.equalsIgnoreCase("m")) {
+            this.ancho = ancho;
+            this.alto = alto;
+            this.fondo = fondo;
+        } else {
+            //Caso invalido.
+            System.out.println("Unidad no válida. Utilice 'cm' o 'm'.");
+
+            this.ancho = 0;
+            this.alto = 0;
+            this.fondo = 0;
+        }
+        this.etiqueta = ""; //definimos también la etiqueta como vacío.
+    }
+
+    public double calcularVolumen() {
+        return this.ancho * this.alto * this.fondo;
+    }
+
+    public double getVolumen() {
+        return calcularVolumen();
+    }
+
+    public void setEtiqueta(String etiqueta) {
+        if (etiqueta.length() <= 30) {
+            this.etiqueta = etiqueta;
+        } else {
+            System.out.println("La etiqueta excede los 30 caracteres.");
+        }
+    }
+
+    public void imprimirEtiqueta() {
+        if (!this.etiqueta.isEmpty()) {
+            System.out.println("Etiqueta:");
+            System.out.println(this.etiqueta);
+        } else {
+            System.out.println("La caja no tiene etiqueta.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Caja de dimensiones: %.2fm x %.2fm x %.2fm, Etiqueta: %s",
+                this.ancho, this.alto, this.fondo, this.etiqueta);
+    }
+}
---
+++ src/Ejercicio8_12/Prueba.java
@@ -0,0 +1,10 @@
+package Ejercicio8_12;
+
+public class Prueba {
+    public static void main(String[] args) {
+        Caja miCaja = new Caja(10, 20, 30, "cm");
+        miCaja.setEtiqueta("Caja de prueba");
+        System.out.println(miCaja);
+    }
+}
+
