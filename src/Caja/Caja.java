18949e0ed6250b7b06b7a0851c0ca1cbbbda7f02
AngelsGV <angels.not99@gmail.com>
2024-03-18 15:58:39 +0100

    Initial commit

---
+++ src/Caja/Caja.java
@@ -0,0 +1,71 @@
+package Caja;
+
+public class Caja {
+    // Definición de parámetros
+    //Por el momento no no les pongo modificador de visibilidad,
+    // pero después debería poner protected para que las clases vecinas los puedan usar.
+    protected double ancho, alto, fondo;
+     protected String etiqueta;
+
+    //Uso double porque m y cm puede estar definido en decimal
+    Caja(double ancho, double alto, double fondo, String unidad ){//constructor caja
+//lo de la unidad lo he buscado. También lo podría hacer con unidad.toLowerCase().equals("cm"))
+        if (unidad.equalsIgnoreCase("cm")) {
+            //comparamos 2 cadenas de caracteres ignorando mayusculas y minusculas.
+
+            this.ancho = ancho / 100;  // Convertir de cm a m
+            this.alto = alto / 100;
+            this.fondo = fondo / 100;
+        } else if (unidad.equalsIgnoreCase("m")) {
+            this.ancho = ancho;
+            this.alto = alto;
+            this.fondo = fondo;
+        } else {
+            //Caso invalido.
+            System.out.println("Unidad no válida. Utilice 'cm' o 'm'.");
+
+            this.ancho = 0;
+            this.alto = 0;
+            this.fondo = 0;
+        }
+        this.etiqueta = ""; //definimos también la etiqueta como vacío.
+    }
+
+    public double getVolumen() {
+        double volumen = ancho*alto*fondo;
+        return volumen;
+    }
+
+    public void setEtiqueta(String etiqueta) {
+        if (etiqueta.length() <= 30) { //igual o menor que 30 caracteres
+            this.etiqueta = etiqueta;
+        } else {//Si tiene más.
+            System.out.println("La etiqueta excede los 30 caracteres.");
+        }
+    }
+
+    public void imprimirEtiqueta() {
+        if (!this.etiqueta.isEmpty()) {
+            System.out.println("Etiqueta:");
+            System.out.println(this.etiqueta);
+        } else {
+            System.out.println("La caja no tiene etiqueta.");
+        }
+    }
+
+    @Override
+    public String toString() { //En este metodo no estoy muy segura de como lo quieres
+
+    return "Dimensiones: " + ancho + " x " + alto + " x " + fondo + ", Etiqueta: " + etiqueta;
+    }
+}
+
+//--------------------------------------------
+// Estoy super atascada, creo que la clase esta más o menos correcta, pero a la hora de ubicar archivos o
+// hacer pruebas el programa me marca los siguientes errores:
+
+//"C:\Program Files\Java\jdk-20\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2023.2.3\lib\idea_rt.jar=63377:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2023.2.3\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 Ejercicio8_12.Prueba
+//Error: Could not find or load main class Ejercicio8_12.Prueba
+//Caused by: java.lang.ClassNotFoundException: Ejercicio8_12.Prueba
+//
+//Process finished with exit code 1
---
+++ src/Caja/Main.java
@@ -0,0 +1,22 @@
+package Caja;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Crear una instancia de la clase Caja
+        Caja caja = new Caja(10, 20, 30, "cm");
+
+        // Establecer una etiqueta para la caja
+        caja.setEtiqueta("Caja de prueba");
+
+        // Imprimir la etiqueta de la caja
+        caja.imprimirEtiqueta();
+
+        // Imprimir la representación de la caja
+        System.out.println(caja);
+
+        // Calcular y mostrar el volumen de la caja
+        System.out.println("Volumen de la caja: " + caja.getVolumen());
+    }
+}
