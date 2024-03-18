a18721c0aa35c206f2d6cd801f7b5cee7b4cbd3d
AngelsGV <angels.not99@gmail.com>
2024-03-18 15:51:13 +0100

    Initial commit

--- src/Ejercicio8_12/Prueba.java
+++ src/Ejercicio8_12/Prueba.java
@@ -1,15 +1,20 @@
 package Ejercicio8_12;
 
-
 public class Prueba {
     public static void main(String[] args) {
+        // Crear una instancia de la clase Caja
         Caja caja = new Caja(10, 20, 30, "cm");
-        double volumen = caja.getVolumen();
-        System.out.println("Volumen de la caja: " + volumen);
 
-        caja.setEtiqueta("Nueva etiqueta para la caja");
+        // Establecer una etiqueta para la caja
+        caja.setEtiqueta("Caja de prueba");
+
+        // Imprimir la etiqueta de la caja
         caja.imprimirEtiqueta();
+
+        // Imprimir la representación de la caja
+        System.out.println(caja);
+
+        // Calcular y mostrar el volumen de la caja
+        System.out.println("Volumen de la caja: " + caja.getVolumen());
     }
 }
-
-
