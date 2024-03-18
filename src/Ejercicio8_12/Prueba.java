8e2260b87205908fa568fedf099e33d3de8af8fd
AngelsGV <angels.not99@gmail.com>
2024-03-18 13:12:26 +0100

    Initial commit

--- src/Ejercicio8_12/Prueba.java
+++ src/Ejercicio8_12/Prueba.java
@@ -2,9 +2,9 @@ package Ejercicio8_12;
 
 public class Prueba {
     public static void main(String[] args) {
-        Caja miCaja = new Caja(10, 20, 30, "cm");
-        miCaja.setEtiqueta("Caja de prueba");
-        System.out.println(miCaja);
+        Caja caja = new Caja(10, 20, 30, "cm");
+        caja.setEtiqueta("Caja de prueba");
+        System.out.println(caja);
     }
 }
 
